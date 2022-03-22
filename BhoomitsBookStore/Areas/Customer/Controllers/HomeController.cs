1b568ca7ccea0a8f3966c87a75fc4d01919e1c65
W0780326 <W0780326@WDSOL0052Z-21>
2022-03-22 11:39:33 -0400

    Last customer area controller added to homecontroller

--- BhoomitsBookStore/Areas/Customer/Controllers/HomeController.cs
+++ BhoomitsBookStore/Areas/Customer/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BhoomitsBookStore.Controllers
+namespace BhoomitsBookStore.Area.Customer.Controllers
 {
     //Explicitly define that the controller is in the Customer Area
     [Area("Customer")]
