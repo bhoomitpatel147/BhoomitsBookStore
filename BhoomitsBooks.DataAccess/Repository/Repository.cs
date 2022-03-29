40bd0c8606f67c06a9628dd284bc72afd6a3f5df
bhoomitpatel147 <90571835+bhoomitpatel147@users.noreply.github.com>
2022-03-28 23:07:14 -0400

    Default Repository file added

--- BhoomitsBooks.DataAccess/Repository/Repository.cs
+++ BhoomitsBooks.DataAccess/Repository/Repository.cs
@@ -10,18 +10,83 @@ using System.Threading.Tasks;
 
 namespace BhoomitsBooks.DataAccess.Repository
 {
+    // Implements all the methods of the IRepository
     public class Repository<T> : IRepository<T> where T : class
     {
-        private readonly ApplicationDbContext _db;
+        // modify the database w/ the db context
+        private readonly ApplicationDbContext _db;      // get the db instance using the constructor and DI 
         internal DbSet<T> dbSet;
-
-        public Repository(ApplicationDbContext db)
+        public Repository(ApplicationDbContext db)     // use hot keys C-T-O-R to build the constructor
         {
             _db = db;
             this.dbSet = _db.Set<T>();
-
+        }
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);      // add context so classes correspond to the DbSet in ApplicationDbContext
         }
 
-        public void Add(T entity);
+        public T Get(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            return query.ToList();      // returns the IEnumerable based on the conditions of the query
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+
+            return query.FirstOrDefault();      // returns the IEnumerable based on the conditions of the query
+        }
+
+        public void Remove(int id)
+        {
+            T entity = dbSet.Find(id);
+            Remove(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
     }
 }
