e0feda54b039b646cba3c6eac266173b4b1b5a41
bhoomitpatel147 <90571835+bhoomitpatel147@users.noreply.github.com>
2022-04-02 02:12:21 -0400

    Product Repository added

---
+++ BhoomitsBooks.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using BhoomitsBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BhoomitsBooks.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        public void Update(Product product);
+        public void Remove(Product objFromDb);
+    }
+}
--- BhoomitsBooks.DataAccess/Repository/UnitOfWork.cs
+++ BhoomitsBooks.DataAccess/Repository/UnitOfWork.cs
@@ -24,6 +24,8 @@ namespace BhoomitsBooks.DataAccess.Repository
         public ISP_Call SP_Call { get; private set; }
         public ICoverTypeRepository CoverType { get; private set; }
 
+        public IProductRepository Product { get; private set; }
+
         public void Dispose()
         {
             _db.Dispose(); 
