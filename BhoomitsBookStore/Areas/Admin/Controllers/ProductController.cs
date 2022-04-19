6f6657f59628736e2334f2a50541da321dd652bc
bhoomitpatel147 <90571835+bhoomitpatel147@users.noreply.github.com>
2022-04-19 15:14:31 -0400

    Image must require completed

--- BhoomitsBookStore/Areas/Admin/Controllers/ProductController.cs
+++ BhoomitsBookStore/Areas/Admin/Controllers/ProductController.cs
@@ -1,11 +1,11 @@
-﻿using BhoomitsBooks.DataAccess.Repository.IRepository;
+﻿
+using BhoomitsBooks.DataAccess.Repository.IRepository;
 using BhoomitsBooks.Models;
 using BhoomitsBooks.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Hosting;
 using System;
-using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,7 +17,6 @@ namespace BhoomitsBookStore.Areas.Admin.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IWebHostEnvironment _hostEnvironment;
-
         public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
         {
             _unitOfWork = unitOfWork;
@@ -43,8 +42,8 @@ namespace BhoomitsBookStore.Areas.Admin.Controllers
                 {
                     Text = i.Name,
                     Value = i.Id.ToString()
-                })
-            };
+                }),
+            };   // using BhoomitBooks.Models;
             if (id == null)
             {
                 // this is for create
@@ -61,81 +60,35 @@ namespace BhoomitsBookStore.Areas.Admin.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Upsert(ProductVM productVM)
+        public IActionResult Upsert(Product product)
         {
             if (ModelState.IsValid)
             {
-                string webRootPath = _hostEnvironment.WebRootPath;
-                var files = HttpContext.Request.Form.Files;
-                if (files.Count > 0)
+                if (product.Id == 0)
                 {
-                    string fileName = Guid.NewGuid().ToString();
-                    var uploads = Path.Combine(webRootPath, @"images\products");
-                    var extension = Path.GetExtension(files[0].FileName);
-
-                    if (productVM.Product.ImageUrl != null)
-                    {
-                        // this is an edit and we need to remove old image
-                        var imagePath = Path.Combine(webRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
-                        if (System.IO.File.Exists(imagePath))
-                        {
-                            System.IO.File.Delete(imagePath);
-                        }
-                    }
-                    using (var filesStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
-                    {
-                        files[0].CopyTo(filesStreams);
-                    }
-                    productVM.Product.ImageUrl = @"\images\products\" + fileName + extension;
+                    _unitOfWork.Product.Add(product);
                 }
                 else
                 {
-                    // update when they do not change the image
-                    if (productVM.Product.Id != 0)
-                    {
-                        Product objFromDb = _unitOfWork.Product.Get(productVM.Product.Id);
-                        productVM.Product.ImageUrl = objFromDb.ImageUrl;
-                    }
-                }
-
-                if (productVM.Product.Id == 0)
-                {
-                    _unitOfWork.Product.Add(productVM.Product);
-                }
-                else
-                {
-                    _unitOfWork.Product.Update(productVM.Product);
+                    _unitOfWork.Product.Update(product);
                 }
                 _unitOfWork.Save();
                 return RedirectToAction(nameof(Index));
             }
-            else
-            {
-                productVM.CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
-                {
-                    Text = i.Name,
-                    Value = i.Id.ToString()
-                });
-                productVM.CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
-                {
-                    Text = i.Name,
-                    Value = i.Id.ToString()
-                });
-                if (productVM.Product.Id != 0)
-                {
-                    productVM.Product = _unitOfWork.Product.Get(productVM.Product.Id);
-                }
-            }
-            return View(productVM);
+            return View(product);
         }
 
-        #region API CALLS
 
+        // API Calls
+        #region API CALLS
         [HttpGet]
+
         public IActionResult GetAll()
         {
+            //return NotFound();
             var allObj = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
             return Json(new { data = allObj });
+
         }
 
         [HttpDelete]
@@ -144,20 +97,13 @@ namespace BhoomitsBookStore.Areas.Admin.Controllers
             var objFromDb = _unitOfWork.Product.Get(id);
             if (objFromDb == null)
             {
-                return Json(new { success = false, message = "Error while deleting" });
-            }
-            string webRootPath = _hostEnvironment.WebRootPath;
-            var imagePath = Path.Combine(webRootPath, objFromDb.ImageUrl.TrimStart('\\'));
-            if (System.IO.File.Exists(imagePath))
-            {
-                System.IO.File.Delete(imagePath);
+                return Json(new { success = true, message = "Erroe while Deleting" });
             }
             _unitOfWork.Product.Remove(objFromDb);
             _unitOfWork.Save();
             return Json(new { success = true, message = "Delete Successful" });
+
         }
-
         #endregion
-
     }
 }
